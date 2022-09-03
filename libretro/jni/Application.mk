b5e3fe7e774992eb87dbad0ff33e551d7ee251fe
hsoftxl <xl19870217@126.com>
2022-09-03 22:25:01 +0800

    lemdroid

--- libretro/jni/Application.mk
+++ libretro/jni/Application.mk
@@ -1,5 +1,5 @@
 NDK_TOOLCHAIN_VERSION ?= 4.8
-APP_ABI := armeabi-v7a,arm64-v8a,x86
+APP_ABI := armeabi-v7a,arm64-v8a,x86,x86_64
 APP_STL := c++_static
 APP_GNUSTL_CPP_FEATURES := exceptions
 APP_PLATFORM := android-9
