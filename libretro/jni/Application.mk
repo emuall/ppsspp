c1d20f3b9febd09b65d01aa56fe63ea962d61674
sean.xie <xl19870217@126.com>
2024-03-06 21:28:33 +0800

    ok

--- libretro/jni/Application.mk
+++ libretro/jni/Application.mk
@@ -1,5 +1,7 @@
 NDK_TOOLCHAIN_VERSION ?= 4.8
+#NDK_TOOLCHAIN_VERSION := clang
 APP_ABI := armeabi-v7a,arm64-v8a,x86
 APP_STL := c++_static
+#APP_STL := c++_shared
 APP_GNUSTL_CPP_FEATURES := exceptions
-APP_PLATFORM := android-9
+APP_PLATFORM := android-21
